d08f3d68522f54702e4178fafe4b5f9a1a9c27ce
Niek Candaele <niekcandaele@gmail.com>
2017-06-24 13:45:49 +0200

    Inital commit

---
+++ alfred.js
@@ -0,0 +1,14 @@
+/*jshint esversion:6*/
+
+const Commando = require('discord.js-commando');
+const bot = new Commando.Client({
+  owner: '324843053921861634'
+});
+
+bot.registry.registerDefaults();
+bot.registry.registerGroup("random" , "Random");
+bot.registry.registerGroup("music", "Music");
+bot.registry.registerCommandsIn(__dirname + '/commands');
+
+
+bot.login('MzI0ODQzMDUzOTIxODYxNjM0.DCPmQg.UQhAZevJRxhPSd4-nHWm_72EyuM');
